8cff7eec4a0edeb1cc537a91c57bf075525ec960
ShawnHesch <shawnhesch@gmail.com>
2015-03-26 23:19:07 -0400

    UPdated icon

--- ParseStarterProject123/app/build/generated/source/r/debug/com/parse/starter/R.java
+++ ParseStarterProject123/app/build/generated/source/r/debug/com/parse/starter/R.java
@@ -20,9 +20,10 @@ public final class R {
     public static final class drawable {
         public static final int female_icon=0x7f020000;
         public static final int ic_launcher=0x7f020001;
-        public static final int male_icon=0x7f020002;
-        public static final int tablebacking=0x7f020003;
-        public static final int tablecellbacking=0x7f020004;
+        public static final int lock2=0x7f020002;
+        public static final int male_icon=0x7f020003;
+        public static final int tablebacking=0x7f020004;
+        public static final int tablecellbacking=0x7f020005;
     }
     public static final class id {
         public static final int action_settings=0x7f07001c;
